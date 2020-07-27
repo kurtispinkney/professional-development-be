420dd1f5c2168c75fc989a9626174fcc4ae0ad50
Kurtis Pinkney <kurtis.pinkney@gmail.com>
2020-07-27 16:59:55 -0400

    Add test index.html

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "/build")));
 app.get("/hello", (req, res) => res.send("Hey!"));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
+    res.sendFile(path.join(__dirname, "/build/index.html"));
 })
 
 app.listen(8000, () => {
